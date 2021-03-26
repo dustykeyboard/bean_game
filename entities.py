ed44b4e6aa065e4c17e286e31ae527ff354d14a9
Philip Boardman <dustykeyboard@brd.mn>
2021-03-27 09:37:54 +1100

    Separate entities for player, enemies and terrain

--- entities.py
+++ entities.py
@@ -2,65 +2,98 @@ import pygame
 import random
 
 class entity(object):
-    def __init__(self,x,y,width,height,vel,color, resolution, floor=0):
-        self.x = x
-        self.y = y
-        self.floor = floor
-        self.width = width
-        self.height = height
-        self.vel = vel
+    def __init__(self, rect, color):
+        self.type = 'unknown'
+        self.vel = 5
         self.color = color
         self.isDucking = False
-        self.isJumping = False
+        self.isFalling = False
         self.fallingVelocity = 0
-        self.resolution = resolution
 
-    def rect(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(rect)
+        self.isMobile = True
+
+    def fall(self, area):
+        if self.isMobile:
+            self.fallingVelocity += 0.6
+            self.rect.move_ip(0, self.fallingVelocity)
+            if not(area.contains(self.rect)):
+                self.rect.clamp_ip(area)
+                self.isFalling = False
+                self.fallingVelocity = 0
+
+    def checkForCollisions(self, rects):
+        for rect in rects:
+            if self.rect != rect: 
+                if self.rect.colliderect(rect):
+                    return True
+        return False
+
+    def tick(self, area):
+        self.fall(area)
+        self.rect.clamp_ip(area)
+
 
     def draw(self, win):
-        height = self.height
-        y = self.y
-        if self.isDucking:
-            height = self.height / 2
-            y = self.y + height
-        pygame.draw.rect(win, self.color, (self.x, y, self.width, height))
+        pygame.draw.rect(win, self.color, self.rect)
 
 class playerEntity(entity):
+    def __init__(self, rect, color):
+        entity.__init__(self, rect, color)
+        self.health = 5
+
     def speed(self):
         return self.vel - 1 if self.isDucking else self.vel
     
     def moveLeft(self):
-        self.x = max(0, self.x - self.speed())
+        self.rect.move_ip(-self.speed(), 0)
 
     def moveRight(self):
-        self.x = min(self.resolution[0] - self.width, self.x + self.speed())
+        self.rect.move_ip(self.speed(), 0)
     
     def duck(self):
         self.isDucking = True
 
     def jump(self):
-        if not(self.isJumping):
-            self.isJumping = True
+        if not(self.isFalling):
+            self.isFalling = True
             self.fallingVelocity = -8
             if (self.isDucking):
                 self.fallingVelocity = -10
         self.isDucking = False
 
-    def fall(self):
-        if (self.isJumping):
-            self.fallingVelocity += 0.6
-            self.y += self.fallingVelocity
-            if (self.y >= self.floor):
-                self.y = min(self.y, self.floor)
-                self.isJumping = False
-                self.fallingVelocity = 0
+    def checkForCollisions(self, rects):
+        return self.rect.collidelistall(rects)
+    
+    def damaged(self):
+        self.health -= 1
+
+    def alive(self):
+        return self.health > 0
+
 
 class enemyEntity(entity):
-
+    def __init__(self, rect):
+        color = (0, random.randint(100,255), 0)
+        entity.__init__(self, rect, color)
+        self.type = 'enemy'
+        self.vel = 2
+        self.direction = random.randint(0,1) * 2 -1
+    
     def wander(self):
-        random.seed()
-        self.vel = random.randrange(-2,2)
-        self.x += self.vel
-        self.x = max(0, self.x - self.vel)
-        self.x = min(self.resolution[0] - self.width, self.x + self.vel)
+        self.rect.move_ip(self.direction, 0)
+
+    def tick(self, area, rects):
+        self.wander()
+        if not(area.contains(self.rect)):
+            self.direction *= -1
+        if self.checkForCollisions(rects):
+            self.direction *= -1
+        entity.tick(self, area)
+
+
+class terrainEntity(entity):
+    def __init__(self, rect, color):
+        entity.__init__(self, rect, color)
+        self.type = 'terrain'
+        self.isMobile = False
--- main.py
+++ main.py
@@ -1,75 +1,100 @@
 import pygame
-from entities import playerEntity, enemyEntity
+from entities import playerEntity, enemyEntity, terrainEntity
 
 pygame.init()
 pygame.display.set_caption("pygame")
 
 clock = pygame.time.Clock()
-resolution = (640,480)
-scale = 10
-screen = pygame.display.set_mode(resolution)
-floor = resolution[1] * 3 / 4
+area = pygame.Rect(0,0,640,480)
+screen = pygame.display.set_mode((area.right, area.bottom))
 
-player = playerEntity(resolution[0]/2,floor, 10, 20, 5, (255,0,255), resolution, floor)
+# colour palette
+purple = (153,0,153)
+green = (0,204,0)
+brown = (101, 51, 0)
+gold = (255,255,102)
+red = (200,0,0)
 
-enemies = [
-    enemyEntity(resolution[0]*3/4,floor, 10, 20, 5, (255,0,0), resolution),
-    enemyEntity(resolution[0]*7/8,floor, 10, 20, 5, (255,0,0), resolution)
+# player
+player = playerEntity((area.right,0, 10, 20), purple)
+
+entities = [
+    enemyEntity((area.right*0.1,0, 10, 20)),
+    enemyEntity((area.right*0.2,0, 10, 20)),
+    enemyEntity((area.right*0.3,0, 10, 20)),
+    enemyEntity((area.right*0.5,0, 10, 20)),
 ]
 
-def checkCollisions():
-    enemyRects = []
-    for enemy in enemies:
-        enemyRects.append(enemy.rect())
-
-    return player.rect().collidelist(enemyRects)
-
-def redrawGameWindow():
-    screen.fill((0,0,0))
-
-    for enemy in enemies:
-        enemy.draw(screen)
-
-    player.draw(screen)
-
-    pygame.display.update()
+# terrain
+terrain = [
+    terrainEntity((50, 50, 100, 5), brown),
+    terrainEntity((100, 100, 200, 5), brown),
+    terrainEntity((150, 150, 200, 5), brown),
+    terrainEntity((250, 250, 100, 5), brown),
+    terrainEntity((150, 400, 100, 5), brown),
+    terrainEntity((50, 450, 100, 5), brown),
+]
 
 
-running = True
-while running:
-    clock.tick(60)
+def main():
+    running = True
+    while running:
+        clock.tick(60)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_h]:
+            player.moveLeft()
+        if keys[pygame.K_RIGHT] or keys[pygame.K_l]:
+            player.moveRight()
+        if keys[pygame.K_DOWN] or keys[pygame.K_j]:
+            player.duck()
+        if keys[pygame.K_UP] or keys[pygame.K_k]:
+            player.jump()
+        if keys[pygame.K_SPACE]:
+            player.jump()
+
+        # update entities for the frame
+        allRects = []
+        for entity in entities:
+            allRects.append(entity.rect)
+        
+        for entity in entities:
+            if entity.isMobile:
+                entity.tick(area, allRects)
+        
+        player.tick(area)
+        collidedRectIndexes = player.checkForCollisions(allRects)
+        if collidedRectIndexes:
+            for collidedRectIndex in collidedRectIndexes:
+                entities.pop(collidedRectIndex)
+            for collidedRectIndex in collidedRectIndexes:
+                entities.append(enemyEntity((area.right*0.1,0, 10, 20)))
+            player.damaged()
+
+        if not(player.alive()):
             running = False
 
-    # player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] or keys[pygame.K_h]:
-        player.moveLeft()
-    if keys[pygame.K_RIGHT] or keys[pygame.K_l]:
-        player.moveRight()
-    if keys[pygame.K_DOWN] or keys[pygame.K_j]:
-        player.duck()
-    if keys[pygame.K_UP] or keys[pygame.K_k]:
-        player.jump()
-    if keys[pygame.K_SPACE]:
-        player.jump()
+        # render
+        screen.fill((0,0,0))
 
-    # let gravity do it's thing
-    player.fall()
+        for entity in terrain + entities + [player]:
+            entity.draw(screen)
+            
+        font = pygame.font.SysFont(None, 24)
+        img = font.render('Health', True, red)
+        screen.blit(img, (20, 20))
+        font = pygame.font.SysFont(None, 24)
+        img = font.render("*" * player.health, True, red)
+        screen.blit(img, (20, 40))
 
-    # enemies wander
-    for enemy in enemies:
-        enemy.wander()
+        pygame.display.update()
 
-    if checkCollisions() > -1:
-        running = False
+    pygame.quit()
 
-    # render
-    redrawGameWindow()
-
-pygame.quit()
-# # while True:
-# pygame.display.update()
-# screen.blit(cup.image, cup.rect)
+if __name__ == "__main__":
+    main()
